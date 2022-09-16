cb7f09728f7a0c7a330e4af0754d7d96e240aae1
Agnieszka Gregosiewicz <agnieszka.gregosiewicz@gmail.com>
2022-09-16 14:22:25 +0200

    add MainUser

---
+++ src/WorkshopMySQLOOP/MainUser.java
@@ -0,0 +1,38 @@
+package WorkshopMySQLOOP;
+
+import org.w3c.dom.UserDataHandler;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class MainUser {
+    public static void main(String[] args) throws SQLException {
+        MainUser mainUser = new MainUser();
+        User user = new User();
+//        try {
+//            UserDAO.delete(8);
+//        } catch (IndexOutOfBoundsException e) {
+//            System.err.println(e.getMessage());
+//        }
+        user.setName("Jaś");
+        user.setPassword("haslo");
+        user.setEmail("jas@gmail.com");
+        //       UserDAO.create(user);
+        //user.setId(9);
+//        try {
+//            UserDAO.update(user);
+//        } catch (IndexOutOfBoundsException e) {
+//            System.err.println(e.getMessage());
+//        }
+
+//        System.out.println(user.getId());
+//        User user2 = UserDAO.read(4);
+//       System.out.println(user2.toString());
+//        UserDAO.delete(4);
+//        User[] users = UserDAO.findAll();
+//        for (User u : users) {
+//            System.out.println(u);
+//        }
+
+    }
+}
