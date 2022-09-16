822210500180fa294318e8c0e339531588258020
Agnieszka Gregosiewicz <agnieszka.gregosiewicz@gmail.com>
2022-09-16 15:25:17 +0200

    refresh checking method

--- src/WorkshopMySQLOOP/MainUser.java
+++ src/WorkshopMySQLOOP/MainUser.java
@@ -1,7 +1,5 @@
 package WorkshopMySQLOOP;
-
 import org.w3c.dom.UserDataHandler;
-
 import java.sql.SQLException;
 import java.util.Arrays;
 
@@ -9,30 +7,51 @@ public class MainUser {
     public static void main(String[] args) throws SQLException {
         MainUser mainUser = new MainUser();
         User user = new User();
-//        try {
-//            UserDAO.delete(8);
-//        } catch (IndexOutOfBoundsException e) {
-//            System.err.println(e.getMessage());
-//        }
-        user.setName("Jaś");
-        user.setPassword(UserDAO.hashPassword("haslo"));
-        user.setEmail("jas2@gmail.com");
+        /*
+        // Sprawdzanie działania metody create
+        user.setName("Jasio");
+        user.setPassword(UserDAO.hashPassword("haslotrzaslo"));
+        user.setEmail("agagag@gmail.com");
         UserDAO.create(user);
-        //user.setId(9);
-//        try {
-//            UserDAO.update(user);
-//        } catch (IndexOutOfBoundsException e) {
-//            System.err.println(e.getMessage());
-//        }
+        */
 
-//        System.out.println(user.getId());
-//        User user2 = UserDAO.read(4);
-//       System.out.println(user2.toString());
-//        UserDAO.delete(4);
-//        User[] users = UserDAO.findAll();
-//        for (User u : users) {
-//            System.out.println(u);
-//        }
+        /*
+        // Sprawdzanie działania metody update
+        user.setName("Jasio");
+        user.setPassword(UserDAO.hashPassword("haslotrzaslo"));
+        user.setEmail("agagag@gmail.com");
+        user.setId(9);
+        try {
+            UserDAO.update(user);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println(e.getMessage());
+        }
+        */
+
+        /*
+        //Sprawdzanie działania metody read
+        User user2 = UserDAO.read(4);
+       System.out.println(user2.toString());
+         */
+
+        /*
+        //Sprawdzanie działania metody findAll
+
+        User[] users = UserDAO.findAll();
+        for (User u : users) {
+            System.out.println(u);
+        */
+
+         /*
+        // Sprawdzanie działania metody delete
+
+        try {
+           UserDAO.delete(8);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println(e.getMessage());
+       }
+         */
+
 
     }
 }
